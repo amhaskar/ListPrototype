3c31ac57bca4259df49ad023dd250e025b011274
Amit Mhaskar <mhaskar4@gmail.com>
2013-03-17 10:11:40 -0400

    trying to test routing issues

--- js/controllers/controllers.js
+++ js/controllers/controllers.js
@@ -1,3 +1,17 @@
-function BodyController($scope) {
-    $scope.Message = "Nothing to see " + "yet!";
-}
+var app = angular.module('', []).
+    config(['$routeProvider', '$locationProvider', function($routeProvider, $locationProvider) {
+        $locationProvider.html5Mode(true);
+        $routeProvider
+        .when('/search',
+            {
+                templateUrl: "templates/search.html",
+                controller: BodyController
+            })
+        .otherwise({ redirectTo: '/' });
+    }]);
+
+
+var BodyController = function($scope) {
+    var self = this;
+
+};
